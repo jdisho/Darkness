ab80147fddf805516176ec663dc6c2269dac5d43
Joan Disho <dishojoan@gmail.com>
2019-10-17 22:51:32 +0200

    create the initial Xcode project

---
+++ Darkness/Darkness/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Darkness
+//
+//  Created by Joan Disho on 17.10.19.
+//  Copyright © 2019 Joan Disho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Darkness/Darkness/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Darkness
+//
+//  Created by Joan Disho on 17.10.19.
+//  Copyright © 2019 Joan Disho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
