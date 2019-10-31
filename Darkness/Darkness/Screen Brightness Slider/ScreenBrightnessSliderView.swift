3483c47187f3e92d6e302939c5e9e7c72cdc7cf3
Joan Disho <dishojoan@gmail.com>
2019-10-31 13:49:03 +0100

    enable the slider/description when the checkbox is selected

--- Darkness/Darkness/Screen Brightness Slider/ScreenBrightnessSliderView.swift
+++ Darkness/Darkness/Screen Brightness Slider/ScreenBrightnessSliderView.swift
@@ -11,7 +11,26 @@ import AppKit
 
 class ScreenBrightnessSliderView: NSView {
 
-    @IBOutlet private var checkmarkButton: NSButton!
-    @IBOutlet private var slider: NSSlider!
-    @IBOutlet private var descriptionTextField: NSTextField!
+    @IBOutlet private var checkmarkButton: NSButton! {
+        didSet {
+            checkmarkButton.state = .off
+        }
+    }
+
+    @IBOutlet private var slider: NSSlider! {
+        didSet {
+            slider.isEnabled = checkmarkButton.state == .on
+        }
+    }
+
+    @IBOutlet private var descriptionTextField: NSTextField! {
+        didSet {
+            descriptionTextField.textColor = checkmarkButton.state == .on ? .labelColor : .secondaryLabelColor
+        }
+    }
+
+    @IBAction func selectCheckmarkButton(_ sender: NSButton) {
+        slider.isEnabled = sender.state == .on
+        descriptionTextField.textColor = sender.state == .on ? .labelColor : .secondaryLabelColor
+    }
 }
