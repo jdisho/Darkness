d141b54e0dbf4e83c6e1c421eafcc43e1ed43f3d
Joan Disho <dishojoan@gmail.com>
2019-11-16 23:32:28 +0100

    set copyright and version

--- Darkness/Darkness/UI/About/AboutViewController.swift
+++ Darkness/Darkness/UI/About/AboutViewController.swift
@@ -10,7 +10,25 @@ import Cocoa
 
 class AboutViewController: NSViewController {
 
+    @IBOutlet private var versionNumberTextField: NSTextField!
+    @IBOutlet private var copyrightTextField: NSTextField!
+
+    private var copyright: String {
+        let year = Calendar.current.component(.year, from: Date())
+        if year == 2019 {
+             return "© 2019 Joan Disho"
+        }
+        return "© 2019 - \(year) Joan Disho"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if let versionNumber = Bundle.main.versionNumber {
+             versionNumberTextField.stringValue = "Version \(versionNumber)"
+        }
+
+        copyrightTextField.stringValue = copyright
     }
+
 }
